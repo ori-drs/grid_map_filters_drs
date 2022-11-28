43c0d81ffb1c2888a27383db012f35cd6948bce2
Benoit Casseau <benoit@oxfordrobotics.institute>
2022-11-28 17:48:13 +0000

    make unknown areas traversable

--- src/elevation_map_filter/GridMapFilter.cpp
+++ src/elevation_map_filter/GridMapFilter.cpp
@@ -23,6 +23,8 @@ class LidarElevationMapFilter
    private:
    ros::NodeHandle nh_;
    int erosion_size_;
+   std::string input_topic_;
+   std::string output_topic_;
    ros::Publisher filtered_map_pub_;
    ros::Subscriber elevation_map_sub_;
 };
@@ -32,10 +34,12 @@ LidarElevationMapFilter::LidarElevationMapFilter(ros::NodeHandle& nh):
 {
   
   nh_.param("erosion_size", erosion_size_, 2);
+  nh_.param("input_topic", input_topic_, std::string("/elevation_mapping/elevation_map_raw"));
+  nh_.param("output_topic", output_topic_, std::string("/elevation_mapping/elevation_map_filtered"));
 
-  elevation_map_sub_ = nh_.subscribe("/elevation_mapping/elevation_map_raw", 1, &LidarElevationMapFilter::gridMapCallback, this);
+  elevation_map_sub_ = nh_.subscribe(input_topic_, 1, &LidarElevationMapFilter::gridMapCallback, this);
 
-  filtered_map_pub_ = nh_.advertise<grid_map_msgs::GridMap>("/elevation_mapping/elevation_map_filtered", 1, true);
+  filtered_map_pub_ = nh_.advertise<grid_map_msgs::GridMap>(output_topic_, 1, true);
 
 }
 
@@ -50,10 +54,21 @@ void LidarElevationMapFilter::gridMapCallback(const grid_map_msgs::GridMap& msg)
     return;
   }*/
 
+  grid_map::Matrix& data = input_map["traversability"];
+  for (GridMapIterator iterator(input_map); !iterator.isPastEnd(); ++iterator)
+  {
+    const Index index(*iterator);
+    // everyting that is not seen ( Nan ) is traversable
+    if (!std::isfinite(data(index(0), index(1))))
+    {
+      data(index(0), index(1)) = 1.0;
+    }
+  }
+
 
   // Erode the traversable image
   cv::Mat original_image, erode_image;
-  GridMapCvConverter::toImage<unsigned short, 1>(input_map, "traversability", CV_16UC1, 0.0, 0.3, original_image);
+  GridMapCvConverter::toImage<unsigned short, 1>(input_map, "traversability", CV_16UC1, 0.0, 0.5, original_image);
 
   int erode_size =  erosion_size_;
   cv::Mat element = cv::getStructuringElement(cv::MORPH_ELLIPSE,
@@ -62,7 +77,7 @@ void LidarElevationMapFilter::gridMapCallback(const grid_map_msgs::GridMap& msg)
 
   erode(original_image, erode_image, element);
 
-  GridMapCvConverter::addLayerFromImage<unsigned short, 1>(erode_image, "traversability_dilated", input_map, 0.0, 0.3);
+  GridMapCvConverter::addLayerFromImage<unsigned short, 1>(erode_image, "traversability_dilated", input_map, 0.0, 1.0);
 
   // Publish filtered output grid map.
   grid_map_msgs::GridMap output_msg;
